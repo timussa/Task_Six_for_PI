55d7a4c69cd086cb0748154d523a63e4267b3fdc
timussa <timussa@yahoo.com>
2024-12-13 20:52:06 +0300

    CS Project

---
+++ SomoneProject/SomoneProject/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SomeoneProject
+{
+    internal class Program
+    {
+        public static int DoubleNum(int x)
+        {
+            return x * 2;
+        }
+
+        static void Main(string[] args)
+        {
+            int num = 5;
+            Console.WriteLine($"{num} * 2 = {DoubleNum(num)}");
+        }
+
+        
+
+
+    }
+}
